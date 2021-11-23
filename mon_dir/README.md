a22078bc17a85ac05024fce5cec1fd3bba1a4b1d
Stefan Begerad <dialog@swingbe.de>
2021-11-23 12:20:40 -0500

    feat(mon_dir): initial commit

---
+++ mon_dir/README.md
@@ -0,0 +1,5 @@
+# Overview
+This project can serve as starting point for Maven and Java based projects.
+
+# Links
+[source](https://javapapers.com/core-java/monitor-a-folder-using-java/)
---
+++ mon_dir/src/main/java/de/swingbe/mon_dir/Main.java
@@ -0,0 +1,44 @@
+package de.swingbe.mon_dir;
+
+import java.io.IOException;
+import java.nio.file.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException,
+            InterruptedException {
+
+        Path folder = Paths.get("./");
+
+        //obtain WatchService instance using FileSystems class
+        WatchService watchService = FileSystems.getDefault().newWatchService();
+
+        //register path to be watched with this instance
+        folder.register(watchService, StandardWatchEventKinds.ENTRY_CREATE);
+
+        boolean valid = true;
+        do {
+            WatchKey watchKey = watchService.take();
+
+            for (WatchEvent event : watchKey.pollEvents()) {
+                WatchEvent.Kind kind = event.kind();
+
+                if (StandardWatchEventKinds.ENTRY_CREATE.equals(event.kind())) {
+                    String fileName = event.context().toString();
+                    System.out.println("File Created:" + fileName);
+                } else if (StandardWatchEventKinds.ENTRY_MODIFY.equals(event.kind())) {
+                    String fileName = event.context().toString();
+                    System.out.println("File modified:" + fileName);
+                } else if (StandardWatchEventKinds.ENTRY_DELETE.equals(event.kind())) {
+                    String fileName = event.context().toString();
+                    System.out.println("File deleted:" + fileName);
+                } else {
+                    System.out.println("Event unkown");
+                }
+                valid = watchKey.reset();
+
+            }
+        }
+        while (valid);
+    }
+}
