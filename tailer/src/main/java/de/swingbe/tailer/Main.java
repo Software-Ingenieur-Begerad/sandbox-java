62fc139a6700d4cac78eb34656f593c9d0c08a06
Stefan Begerad <dialog@swingbe.de>
2021-11-30 11:13:15 -0500

    feat(tailer): added Logger class

--- tailer/src/main/java/de/swingbe/tailer/Main.java
+++ tailer/src/main/java/de/swingbe/tailer/Main.java
@@ -2,10 +2,13 @@ package de.swingbe.tailer;
 
 import org.apache.commons.io.input.Tailer;
 import org.apache.commons.io.input.TailerListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 
 public class Main {
+    public final static Logger LOG = LoggerFactory.getLogger(Main.class);
 
     //delay between checks of the file for new content in milliseconds
     public static final int DELAY_MILLIS = 500;
@@ -14,12 +17,12 @@ public class Main {
     public static final boolean END = true;
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        LOG.trace("Entering application.");
 
         TailerListener listener = new MyListener();
 
         //create and use a Tailer with a Thread
-        String fileName = "NetPeerManager.log";
+        String fileName = "/opt/npm/NetPeerManager.log";
         //TODO What is the behaviour of the reOpen parameter?
         Tailer tailer = new Tailer(new File(fileName), listener, DELAY_MILLIS, END);
         tailer.run();
@@ -31,6 +34,7 @@ public class Main {
 
         thread.start();
 
+        LOG.trace("Exiting application.");
         return;
     }
 }
--- tailer/src/main/java/de/swingbe/tailer/MyListener.java
+++ tailer/src/main/java/de/swingbe/tailer/MyListener.java
@@ -1,10 +1,14 @@
 package de.swingbe.tailer;
 
 import org.apache.commons.io.input.TailerListenerAdapter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MyListener extends TailerListenerAdapter {
+    public final static Logger LOG = LoggerFactory.getLogger(MyListener.class);
+
     @Override
     public void handle(String line) {
-        System.out.println(line);
+        LOG.debug("{}", line);
     }
 }
