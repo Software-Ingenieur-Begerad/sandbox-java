99bf301fe5f6d63c05cac4ebb1b17c4ff8c27e36
Stefan Begerad <dialog@swingbe.de>
2022-01-06 13:53:35 -0500

    feat: add rolling-log-slf4j

--- rolling-log/src/main/java/de/swingbe/rollinglog/Main.java
+++ rolling-log/src/main/java/de/swingbe/rollinglog/Main.java
@@ -1,15 +1,20 @@
 package de.swingbe.rollinglog;
 
-import org.apache.logging.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Main {
 
-    private static final Logger logger = Logger.getLogger(Main.class);
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) throws InterruptedException {
         for (int i = 0; i < 2000; i++) {
-            logger.info("This is the " + i + ". time I say 'Hello World'.");
-            Thread.sleep(1);
+            logger.trace("TRACE: This is the " + i + ". time I say 'Hello World'.");
+            logger.info("INFO: This is the " + i + ". time I say 'Hello World'.");
+            logger.debug("DEBUG: This is the " + i + ". time I say 'Hello World'.");
+            logger.warn("WARN: This is the " + i + ". time I say 'Hello World'.");
+            logger.error("ERROR: This is the " + i + ". time I say 'Hello World'.");
+            Thread.sleep(5);
         }
     }
 }
