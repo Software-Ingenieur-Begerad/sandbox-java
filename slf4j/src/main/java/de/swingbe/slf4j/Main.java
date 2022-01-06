a1190e87539b0ef747e5113dcf70ed852d3c6d21
Stefan Begerad <dialog@swingbe.de>
2022-01-06 15:08:38 -0500

    feat: add example logs

--- slf4j/src/main/java/de/swingbe/slf4j/Main.java
+++ slf4j/src/main/java/de/swingbe/slf4j/Main.java
@@ -6,7 +6,7 @@ import org.slf4j.LoggerFactory;
 public class Main {
     public final static Logger LOG = LoggerFactory.getLogger(Main.class);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.out.println("Hello world!");
 
         LOG.debug("Debug Message Logged !!!");
@@ -25,6 +25,14 @@ public class Main {
         }
         LOG.trace("Exiting application.");
 
+        for (int i = 0; i < 2000; i++) {
+            LOG.trace("TRACE: This is the " + i + ". time I say 'Hello World'.");
+            LOG.info("INFO: This is the " + i + ". time I say 'Hello World'.");
+            LOG.debug("DEBUG: This is the " + i + ". time I say 'Hello World'.");
+            LOG.warn("WARN: This is the " + i + ". time I say 'Hello World'.");
+            LOG.error("ERROR: This is the " + i + ". time I say 'Hello World'.");
+            Thread.sleep(20);
+        }
         return;
     }
 }
