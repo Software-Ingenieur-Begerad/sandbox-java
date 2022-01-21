c81976427bbaa3a96953fa7512e726b3e7b7c566
Begerad, Stefan <dialog@swingbe.de>
2022-01-21 17:00:15 -0500

    feat: add logging library

--- ahc/src/main/java/de/swingbe/ahc/Main.java
+++ ahc/src/main/java/de/swingbe/ahc/Main.java
@@ -3,8 +3,13 @@ package de.swingbe.ahc;
 import org.dedriver.TxFactory;
 import org.dedriver.model.Adr;
 import org.dedriver.model.Msg;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Main {
+
+
+    private final static Logger LOG = LoggerFactory.getLogger(Main.class);
 
     /**
      * static String URL = "http://83.223.94.182";
@@ -17,10 +22,14 @@ public class Main {
     static String ROUTE = "/postdata";
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        TxFactory.createTx().send(new Msg("uuuid", "87.263783", "52.9019052",
-                "1642802000000", "alias", "0"), new Adr(PORT, ROUTE, URL));
-        System.out.println("Done!");
+        LOG.debug("Hello world!");
+        Msg msg = new Msg("uuuid", "87.263783", "52.9019052",
+                "1642802000000", "alias", "0");
+        LOG.debug("msg: " + msg);
+        Adr adr = new Adr(PORT, ROUTE, URL);
+        LOG.debug("adr: " + adr);
+        TxFactory.createTx().send(msg, adr);
+        LOG.debug("Done!");
     }
 
 }
