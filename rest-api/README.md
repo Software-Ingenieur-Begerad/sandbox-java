d03f366ac6accc65bc76d23c968b6f62ae3c32a5
Begerad, Stefan <dialog@swingbe.de>
2022-03-28 20:32:43 +0200

    feat(rest-api): initial commit: NOT working

---
+++ rest-api/README.md
@@ -0,0 +1,6 @@
+# Overview
+This project can serve as starting point for a REST API.
+
+# Links
+
+[Creating a REST API](https://happycoding.io/tutorials/java-server/rest-api)
---
+++ rest-api/src/main/java/de/swingbe/rest_api/Main.java
@@ -0,0 +1,9 @@
+package de.swingbe.rest_api;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        return;
+    }
+}
